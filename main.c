bb73289a85850f72aadceb44bde0804106fbcd94
Maximilian Müller <39367548+D3B453R@users.noreply.github.com>
2020-04-28 23:56:14 +0200

    Uploaded Files

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+    int iZahl1=1, iZahl2=0, i=0;
+
+    iZahl1=1;
+    iZahl2=0;
+    //do while
+    do
+    {
+        iZahl1=iZahl1+iZahl2;
+        iZahl2 ++;
+        i++;
+        printf("%d, ", iZahl1);
+        Sleep(1000);
+    }while(i<9);
+
+    //for
+    iZahl1=1;
+    iZahl2=0;
+    printf("\n\n");
+
+for(i=0; i<9; i++) {
+	iZahl1=iZahl1+iZahl2;
+        iZahl2 ++;
+        printf("%d, ", iZahl1);
+}
+printf("\n\n");
+    return 0;
+}
